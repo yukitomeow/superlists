8643e4a785f5f1a7bcf860d995d28b6333a6ca8d
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-11-27 16:18:23 +0900

    changed functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox() #everytime test run this run
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_canstart_a_list_and_retrive_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)  #test assertion
+        self.fail('Finish the test!')   #reminder finishing test
+           
+    if __name__ == "__main__":#このPythonファイルが「python ファイル名.py というふうに実行されているかどうか」を判定するif文です。
+        unittest.main(warnings='ignore')
